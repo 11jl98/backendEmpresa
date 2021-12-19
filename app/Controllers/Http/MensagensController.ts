2d6fce9dc2f22a0bd2d57d2dde1f1cd4dff40d65
EriksJunior <EriksJunior13@gmail.com>
2021-12-19 20:55:40 -0300

    criado controller, validados e a rota para salvar msg

--- app/Controllers/Http/MensagensController.ts
+++ app/Controllers/Http/MensagensController.ts
@@ -1,24 +1,40 @@
 import { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
+import Mensagem from '../../Models/Mensagem'
+import MensagemValidator from '../../Validators/MensagemValidator'
+import { v4 as uuid } from 'uuid'
 
 export default class MensagensController {
-  public async index ({}: HttpContextContract) {
+  public async index({ }: HttpContextContract) {
   }
 
-  public async create ({}: HttpContextContract) {
+  public async create({ }: HttpContextContract) {
   }
 
-  public async store ({}: HttpContextContract) {
+  public async store({ request, auth }: HttpContextContract) {
+    const user = await auth.authenticate()
+    const id = user.id
+
+    const Uuid = uuid();
+    const data = await request.validate(MensagemValidator.MensagemValidatorStore);
+    const mensagem = await Mensagem.create({
+      idEmpresa: id,
+      id_mensagem: Uuid,
+      ...data
+    })
+    mensagem.id_mensagem = Uuid
+
+    return mensagem
   }
 
-  public async show ({}: HttpContextContract) {
+  public async show({ }: HttpContextContract) {
   }
 
-  public async edit ({}: HttpContextContract) {
+  public async edit({ }: HttpContextContract) {
   }
 
-  public async update ({}: HttpContextContract) {
+  public async update({ }: HttpContextContract) {
   }
 
-  public async destroy ({}: HttpContextContract) {
+  public async destroy({ }: HttpContextContract) {
   }
 }
