dbb13bd31075dfe54f09e1a8e40d108a703480e7
11jl98 <joaoferreira981011@gmail.com>
2021-12-20 08:46:43 -0300

    finalizado inventario de estoque

--- app/Controllers/Http/InventarioestoquesController.ts
+++ app/Controllers/Http/InventarioestoquesController.ts
@@ -4,32 +4,35 @@ import Database from '@ioc:Adonis/Lucid/Database'
 
 export default class InventarioestoquesController {
   public async index({ auth}: HttpContextContract) {
-
     const user = await auth.authenticate()
     const id = user.id
-    
-    const estoque = await Database.knexRawQuery(`select
-      estoque.nomeagrotoxico, estoque.id_agrotoxico, estoque.nomeembalagem, estoque.tipoembalagem,
-       estoque.unidademmbalagem, SUM(estoque.quantidade) as total from movimentaestoques as estoque,
-        lotes as lote  where estoque.id_agrotoxico = lote.id_agrotoxico
-         and estoque.nomeembalagem = lote.embalagem and estoque.tipoembalagem = lote.capacidadeembalagem
-          and estoque.unidademmbalagem = lote.unidadeembalagem GROUP BY id_agrotoxico, estoque.nomeembalagem, 
-          estoque.tipoembalagem, estoque.unidademmbalagem` )
-  
-    console.log(estoque[0])
-    return estoque[0]
+    const estoque = await Estoque.query().
+    where('id_empresa','=', id)
+    let count = 1
+    console.log(estoque)
+    const est = estoque.reduce((acc, current) => {
+      let nomeEmbalagem = current.nomeembalagem + ' ' + current.tipoembalagem + ' '+  current.unidademmbalagem
+      acc[current.nomeagrotoxico] = acc[current.nomeagrotoxico] || {}
+      acc[current.nomeagrotoxico][nomeEmbalagem] = acc[current.nomeagrotoxico][nomeEmbalagem] ||{}
+      acc[current.nomeagrotoxico][nomeEmbalagem]['quantidade'] = acc[current.nomeagrotoxico][nomeEmbalagem]['quantidade']||0 
+
+      acc[current.nomeagrotoxico][nomeEmbalagem]['quantidade'] += Number(current.quantidade)
+      acc[current.nomeagrotoxico][nomeEmbalagem]['indice'] = count
+      count = count + 1
+      return acc
+    }, {})
+    return est
   }
   public async indexLote({ auth}: HttpContextContract) {
     const user = await auth.authenticate()
     const id = user.id
     
     const estoque = await Database.knexRawQuery(`select estoque.numlote,
-      estoque.nomeagrotoxico, estoque.id_agrotoxico, estoque.nomeembalagem, estoque.tipoembalagem,
-       estoque.unidademmbalagem, SUM(estoque.quantidade) as total from movimentaestoques as estoque,
-        lotes as lote  where estoque.numlote = lote.numlote and estoque.id_agrotoxico = lote.id_agrotoxico
-         and estoque.nomeembalagem = lote.embalagem and estoque.tipoembalagem = lote.capacidadeembalagem
-          and estoque.unidademmbalagem = lote.unidadeembalagem GROUP BY numlote, estoque.nomeembalagem, 
-          estoque.tipoembalagem, estoque.unidademmbalagem` )
+    estoque.nomeagrotoxico, estoque.id_agrotoxico, estoque.nomeembalagem, estoque.tipoembalagem,
+     estoque.unidademmbalagem, SUM(estoque.quantidade) as total from movimentaestoques as estoque,
+      lotes as lote  where estoque.id_empresa = '${id}' and estoque.numlote = lote.numlote and estoque.id_agrotoxico = lote.id_agrotoxico
+       and estoque.nomeembalagem = lote.embalagem and estoque.tipoembalagem = lote.capacidadeembalagem
+        and estoque.unidademmbalagem = lote.unidadeembalagem GROUP BY numlote, estoque.nomeembalagem, estoque.tipoembalagem, estoque.unidademmbalagem` )
   
     console.log(estoque[0])
     return estoque[0]
