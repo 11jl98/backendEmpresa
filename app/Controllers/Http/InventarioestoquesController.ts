84977535e0522269469c8a9fe733d074c4922859
eriks junior <eriksjunior13@gmail.com>
2021-12-22 14:47:44 -0300

    commit

--- app/Controllers/Http/InventarioestoquesController.ts
+++ app/Controllers/Http/InventarioestoquesController.ts
@@ -3,31 +3,31 @@ import Estoque from 'App/Models/Movimentaestoque'
 import Database from '@ioc:Adonis/Lucid/Database'
 
 export default class InventarioestoquesController {
-  public async index({ auth}: HttpContextContract) {
+  public async index({ params, auth}: HttpContextContract) {
     const user = await auth.authenticate()
-    const id = user.id
-    const estoque = await Estoque.query().
-    where('id_empresa','=', id)
-    let count = 1
-    console.log(estoque)
-    const est = estoque.reduce((acc, current) => {
-      let nomeEmbalagem = current.nomeembalagem + ' ' + current.tipoembalagem + ' '+  current.unidademmbalagem
-      acc[current.nomeagrotoxico] = acc[current.nomeagrotoxico] || {}
-      acc[current.nomeagrotoxico][nomeEmbalagem] = acc[current.nomeagrotoxico][nomeEmbalagem] ||{}
-      acc[current.nomeagrotoxico][nomeEmbalagem]['quantidade'] = acc[current.nomeagrotoxico][nomeEmbalagem]['quantidade']||0 
+    const id = user.id  
 
-      acc[current.nomeagrotoxico][nomeEmbalagem]['quantidade'] += Number(current.quantidade)
-      acc[current.nomeagrotoxico][nomeEmbalagem]['indice'] = count
-      count = count + 1
-      return acc
-    }, {})
-    return est
+    const filtro = params.filtro
+    let texto = params.texto
+
+    texto = decodeURIComponent(texto)
+
+    const dadosPesquisa = await Estoque
+    .query()
+    .select('numlote', 'nomeagrotoxico', 'id_agrotoxico', 'nomeembalagem', 'tipoembalagem', 'unidademmbalagem', 'datavencimento')
+    .sum('quantidade as total')
+    .where('id_empresa', '=', id)
+    .andWhere(filtro, 'like', `%${texto}%`)
+    .groupBy('numlote', 'nomeembalagem', 'tipoembalagem', 'unidademmbalagem')
+    .orderBy('nomeagrotoxico')
+        return dadosPesquisa
+
   }
   public async indexLote({ auth}: HttpContextContract) {
     const user = await auth.authenticate()
     const id = user.id
     
-    const estoque = await Database.knexRawQuery(`select estoque.numlote,
+    const estoque = await Database.knexRawQuery(`select estoque.numlote, estoque.datavencimento,
     estoque.nomeagrotoxico, estoque.id_agrotoxico, estoque.nomeembalagem, estoque.tipoembalagem,
      estoque.unidademmbalagem, SUM(estoque.quantidade) as total from movimentaestoques as estoque,
       lotes as lote  where estoque.id_empresa = '${id}' and estoque.numlote = lote.numlote and estoque.id_agrotoxico = lote.id_agrotoxico
--- start/routes/inventario.ts
+++ start/routes/inventario.ts
@@ -1,6 +1,6 @@
 import Route from '@ioc:Adonis/Core/Route'
 
-Route.get('/inventario/quantidade','InventarioestoquesController.index')
+Route.get('/inventario/relatorio/gerar/:filtro/:texto','InventarioestoquesController.index')
 Route.get('/inventario/quantidade/lote','InventarioestoquesController.indexLote')
 Route.get('/inventario/list','InventarioestoquesController.show')
 Route.get('/inventario/list/:dataInit/:dataFinal','InventarioestoquesController.showDate')
